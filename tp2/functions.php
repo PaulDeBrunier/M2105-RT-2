569db33a40fa6b113039439459471da320d51533
paul <21509204@etu.unicaen.fr>
2016-03-17 16:45:19 +0100

    JEANBON

---
+++ tp2/functions.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Etudiant
+ * Date: 17/03/2016
+ * Time: 16:18
+ */
+
+function getHeader($startSession,$title){
+    if ($startSession)
+        session_start();
+    echo "
+    <!doctype html>
+    <html>
+    <head>
+      <meta charset=''utf-8'>
+      <title>{$title}</title>
+    </head>
+}   <body>"
+function get Footer(){
+    echo"</body></html>";
+}

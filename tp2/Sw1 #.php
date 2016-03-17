4f897bde84d1de669390f08e641081e91d398be5
paul <21509204@etu.unicaen.fr>
2016-03-17 17:33:02 +0100

    JEANBON

--- tp2/Sw1 #.php
+++ tp2/Sw1 #.php
@@ -14,13 +14,15 @@ getHeader(true,"EXERCICUN");
             <legend>Connexion</legend>
             <div class="alert alert-info">Saisir votre pseudonyme ou le nom de votre tante du frére de la mère Caspienne</div>
             <div class="form-group">
-                <input placeholder="Nom..." class=""
-
-
+                <input placeholder="Nom..." class="form-control" type="text" name="nom">
+                <input class="btn btn-primry" type="submit" value="Valider">
             </div>
         </fieldset>
-
     </form>
+</div>
+<?php
+getFooter();
+?>
 
 
 

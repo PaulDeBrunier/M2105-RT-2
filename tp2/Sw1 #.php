0b0e204a4554fa55131b1408cc66bf1e8999a36f
paul <21509204@etu.unicaen.fr>
2016-03-17 17:29:50 +0100

    JEANBON

---
+++ tp2/Sw1 #.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Etudiant
+ * Date: 17/03/2016
+ * Time: 17:13
+ */
+include './../functions.php';
+getHeader(true,"EXERCICUN");
+?>
+<div class="container">
+    <form class="container">
+        <fieldset>
+            <legend>Connexion</legend>
+            <div class="alert alert-info">Saisir votre pseudonyme ou le nom de votre tante du frére de la mère Caspienne</div>
+            <div class="form-group">
+                <input placeholder="Nom..." class=""
+
+
+            </div>
+        </fieldset>
+
+    </form>
+
+
+
+
+
+
+
+
+
+</div>
+

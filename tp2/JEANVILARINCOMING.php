9bb5261861bd3b3f74e0ddcbe80c9f572128bf40
Toph3rZz <21510120@etu.unicaen.fr>
2016-03-24 15:40:54 +0100

    TP2

--- tp2/JEANVILARINCOMING.php
+++ tp2/JEANVILARINCOMING.php
@@ -1,23 +1,31 @@
 <?php
-include './../functions.php';
-getHeader(true,"Excercie 3");
-if(array_key_exists)()
-
 /**
- * Created by PhpStorm.
- * User: Etudiant
- * Date: 24/03/2016
- * Time: 14:35
- */
+* Created by PhpStorm.
+* User: Polo
+* Date: 24/03/2016
+* Time: 15:34
+*/
+include './../functions.php';
+if(array_key_exists("nom",$_COOKIE))
+    header("location:recoAuto.php");
+getHeader(true,"Exercice 2");
 ?>
     <div class="container">
-        <form method=post" action="<php echo $_SERVER{"PHP-SELF}?>">
+        <form class="form-inline" method="post" action="ex2.php">
             <fieldset>
-                <legend>Meilleur navigateur ?</legend>
-                    </fieldset>
-                    <div>
-                         <label><input> name="Navigateur" type="radio" value="Chrome"> Chrome </label>
-                    </div>
-                    <div>
-                        <label><input> name="Navigateur" type="radio" value="Mozilla"> Chrome </label>
-                    </div>
+                <legend>Veuillez vous connecter</legend>
+                <div class="alert alert-info">Saisir votre nom et valider.</div>
+                <div class="form-group">
+                    <input placeholder="Nom..." class="form-control" type="text" name="nom">
+                    <input class="btn btn-primary" type="submit" value="Valider">
+                </div>
+                <div class="form-group">
+                    <label class="control-label"><input type="checkbox" name="ckRemember"> Se souvenir de moi</label>
+                </div>
+            </fieldset>
+
+        </form>
+    </div>
+
+<?php
+getFooter();

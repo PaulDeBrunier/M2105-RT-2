9d0f10c7bace16bdd645d40f4719bd334ee154ac
Toph3rZz <21510120@etu.unicaen.fr>
2016-03-24 15:09:58 +0100

    TP2

---
+++ tp2/JEANVILARINCOMING.php
@@ -0,0 +1,23 @@
+<?php
+include './../functions.php';
+getHeader(true,"Excercie 3");
+if(array_key_exists)()
+
+/**
+ * Created by PhpStorm.
+ * User: Etudiant
+ * Date: 24/03/2016
+ * Time: 14:35
+ */
+?>
+    <div class="container">
+        <form method=post" action="<php echo $_SERVER{"PHP-SELF}?>">
+            <fieldset>
+                <legend>Meilleur navigateur ?</legend>
+                    </fieldset>
+                    <div>
+                         <label><input> name="Navigateur" type="radio" value="Chrome"> Chrome </label>
+                    </div>
+                    <div>
+                        <label><input> name="Navigateur" type="radio" value="Mozilla"> Chrome </label>
+                    </div>

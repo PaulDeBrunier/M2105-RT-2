29a7e8f46a4cd10932934b3bb2a6c45342378f1e
Toph3rZz <21510120@etu.unicaen.fr>
2016-03-24 16:32:34 +0100

    TP2

--- tp2/Excercie 3.php
+++ tp2/Excercie 3.php
@@ -16,11 +16,18 @@ if(isset($_POST['browser'])) {$_SESSION['browser'] = $_POST['browser'];
 <form method="post" action="ex3.php">
     <fieldset>
         <legend> Question n°1 </legend>
-        </fieldset>
+        <fieldset>
             <legend> Votre navigateur favori ? ?</legend>
             <input name="browser" value="Chrome" type="radio"> <label>Chrome</label>
             <input name="browser" value="Mozilla" type="radio"> <label>Mozilla</label>
             <input name="browser" value="Internet Explorer" type="radio"> <label>Internet Explorer</label>
             <input name="browser" value="Opera" type="radio"> <label>Opera</label>
             <input name="browser" value="Autre" type="Autre"> <label>Autre</label>
-        
+        </fieldset>
+        <input type="submit" />
+    </fieldset>
+</form>
+
+<?php
+getFooter();
+?>
